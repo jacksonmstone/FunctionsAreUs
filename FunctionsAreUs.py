516f7dc3cdf1fcca38a2b44d29562da1c6d35fdf
DanielTsao0103 <63975005+DanielTsao0103@users.noreply.github.com>
2025-02-27 12:31:09 -0700

    testing

--- FunctionsAreUs.py
+++ FunctionsAreUs.py
@@ -1,1 +1,10 @@
 #this is the functions page
+# 3. Display list of all teams and allow the user to choose a team using a menu.
+#  Call the function again to let the user choose the opponent but do not display -
+# the team they chose previously. Remove that team from the list.
+#  Allow the user to select an opponent, and return team name. 
+# This function should receive a parameter but give it a default value if none is passed.
+#  You can use this function for both choosing the home team and the opponent team.
+
+#make a data set for the project to start
+#
