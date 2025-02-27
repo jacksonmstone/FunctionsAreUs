c1393033b0cc3b0342195614ea421cd9d011aea3
DanielTsao0103 <63975005+DanielTsao0103@users.noreply.github.com>
2025-02-27 15:20:48 -0700

    starting to put it all together

--- FunctionsAreUs.py
+++ FunctionsAreUs.py
@@ -1,1 +1,10 @@
+from function4 import results
+from functionOne import intro
+from Function3 import *
+teams = []
+teams = ["BYU Cougars", "University of Utah Utes", "Utah State Aggies", "Weber State Wildcats", "UVU Wolverines"]
+#greet the player
+playername = intro()
+def main():
+    printTeams()
 
