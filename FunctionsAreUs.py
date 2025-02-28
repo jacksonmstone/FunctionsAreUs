5620bf85f28a362fef5f2762aa7bbbfa8ee37b02
DanielTsao0103 <63975005+DanielTsao0103@users.noreply.github.com>
2025-02-27 21:04:12 -0700

    changing function2

--- FunctionsAreUs.py
+++ FunctionsAreUs.py
@@ -11,36 +11,37 @@ UserName = intro()
 
 run = playorno(UserName)
 
-if run == True:
-    #print out all the teams on my list
-    print("Here are all the women's soccer teams for the 2025 season!")
-    printTeams(teams)
+for icount in range ():
+    if run == True:
+        #print out all the teams on my list
+        print("Here are all the women's soccer teams for the 2025 season!")
+        printTeams(teams)
 
 
-    #get the number team we are looking at
-    print("What team are we looking at this season?")
-    teamNum = getNumber(teams)
-    myTeam = teams[teamNum] #save our team name
-    teams.pop(teamNum) #need to adjust our list to take out the team we selected
+        #get the number team we are looking at
+        print("What team are we looking at this season?")
+        teamNum = getNumber(teams)
+        myTeam = teams[teamNum] #save our team name
+        teams.pop(teamNum) #need to adjust our list to take out the team we selected
 
-    print("\n")
-    #print out all the remaining teams
-    printTeams(teams)
-
-
-    #get the number team we want to play
-    print("What team would you like to play this season?")
-    teamNum = getNumber(teams)
-    oppTeam = teams[teamNum] 
+        print("\n")
+        #print out all the remaining teams
+        printTeams(teams)
 
 
-    #print the teams we selected
+        #get the number team we want to play
+        print("What team would you like to play this season?")
+        teamNum = getNumber(teams)
+        oppTeam = teams[teamNum] 
 
-    myscore, awayscore = results(myTeam, oppTeam)
-    job = winloss(myscore,awayscore)
-    
-    print(f"My team: {myTeam} {myscore}")
-    print(f"Opposing team: {oppTeam} {awayscore}")
-    print(f"your team came back with a {job}")
+
+        #print the teams we selected
+
+        myscore, awayscore = results(myTeam, oppTeam)
+        job = winloss(myscore,awayscore)
+        
+        print(f"My team: {myTeam} {myscore}")
+        print(f"Opposing team: {oppTeam} {awayscore}")
+        print(f"your team came back with a {job}")
 
 
